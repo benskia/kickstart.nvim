760fd04e2c75939d2e3beda90de8e7df831dbc08
Abraham Benski <AbrahamBenski@proton.me>
2025-03-14 12:21:17 -0500

    colorscheme catppuccin-mocha

--- init.lua
+++ init.lua
@@ -791,20 +791,24 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'rose-pine/neovim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('rose-pine').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
+      require('catppuccin').setup {
+        -- color_overrides = {
+        --   mocha = {
+        --     base = '#0F0F0F',
+        --     mantle = '#0F0F0F',
+        --     crust = '#0F0F0F',
+        --   },
+        -- },
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'rose-pine'
+      vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- Remove background.
       vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
